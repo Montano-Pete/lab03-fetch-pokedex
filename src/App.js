09b75adf8ca6823a6f2cd789b28045701dd1ab90
Peter <pete225848@gmail.com>
2021-06-10 15:56:44 -0700

    renders out pokemon image and stats on page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react'
 import request from 'superagent';
 import './App.css';
+import PokeList from './PokeList.js';
 
 export default class App extends Component {
-  handleFetch = async () => {
-    await request.get('https://pokedex-alchemy.herokuapp.com/api/pokedex')
+  state = {
+    pokedex: []
+  }
+  
+  fetchDex = async () => {
+    const dex = await request.get('https://pokedex-alchemy.herokuapp.com/api/pokedex');
+
+    this.setState({ pokedex: dex.body.results })
+
   }
   render() {
     return (
-      <div>
-        <button onClick={this.handleFetch}>Fetch Pokedex!</button>
+      <div className="App">
+        <button onClick={this.fetchDex}>Fetch Pokedex!</button>
+        <PokeList pokedex={this.state.pokedex}/>
       </div>
     )
   }
---
+++ src/PokeItem.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class PokeItem extends Component {
+    render() {
+        return (
+            <div className="card">
+               <h2>{this.props.pokemon}</h2>
+               <img src={this.props.image} alt=''/>
+               <p>Attack: {this.props.attack}</p>
+               <p>Defense: {this.props.defense}</p>
+               <p>Type: {this.props.type}</p>
+            </div>
+        )
+    }
+}

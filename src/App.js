7a876d96816fdb31913415f03c122a026b67eb81
Peter <pete225848@gmail.com>
2021-06-10 13:30:58 -0700

    successfully fetch pokedex

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import request from 'superagent';
 import './App.css';
 
 export default class App extends Component {
+  handleFetch = async () => {
+    await request.get('https://pokedex-alchemy.herokuapp.com/api/pokedex')
+  }
   render() {
     return (
       <div>
+        <button onClick={this.handleFetch}>Fetch Pokedex!</button>
       </div>
     )
   }

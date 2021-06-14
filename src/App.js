52b0ba378d27903e75d7120b221f8c38308e9c79
Peter <pete225848@gmail.com>
2021-06-14 14:14:22 -0700

    Implemented loading state

--- src/App.js
+++ src/App.js
@@ -5,19 +5,31 @@ import PokeList from './PokeList.js';
 
 export default class App extends Component {
   state = {
-    pokedex: []
+    pokedex: [],
+    loading: false,
   }
   
+  componentDidMount = async () => {
+    this.setState({ loading: true });
+    const dex = await request.get('https://pokedex-alchemy.herokuapp.com/api/pokedex')
+
+    this.setState({ loading: false });
+    this.setState({ pokedex: dex.body.results });
+  }
+
+
   fetchDex = async () => {
-    const dex = await request.get('https://pokedex-alchemy.herokuapp.com/api/pokedex');
+    this.setState({ loading: true });
+    const dex = await request.get('https://pokedex-alchemy.herokuapp.com/api/pokedex')
 
-    this.setState({ pokedex: dex.body.results })
-
+    this.setState({ loading: false });
+    this.setState({ pokedex: dex.body.results });
   }
   render() {
     return (
       <div className="App">
         <button onClick={this.fetchDex}>Fetch Pokedex!</button>
+        {this.state.loading && <h1>LOADING!!!</h1>}
         <PokeList pokedex={this.state.pokedex}/>
       </div>
     )
